5a95796d830b1a5e077d95c381946e245484bfd4
Glenda <70830716+glendasalas7@users.noreply.github.com>
2022-03-14 00:16:35 -0700

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -4,11 +4,5 @@ from firebase_admin import credentials, firestore, storage
 ## privateKey must be from your Firebase project
 cred = credentials.Certificate("./serviceAccountKey.json")
 
-## change storageBucket according to your Firebase project
-firebase_admin.initialize_app(cred, {
-    "storageBucket": "cmsc-4313-iot.appspot.com"
-})
-
 ## Firestore and Cloud Storage access
 db = firestore.client()
-bucket = storage.bucket()
